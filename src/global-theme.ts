ecb57fe5c89fb40c7df5c168820c28e91ded236e
Dominik Stumpf <s.dominik.work@gmail.com>
2023-07-13 15:40:31 +0200

    nextjs migration base setup

--- src/global-theme.ts
+++
@@ -1,8 +0,0 @@
-export const globalTheme = {
-  headline: "Bitter, Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
-  body: "Raleway, Arial, Helvetica, sans-serif",
-  primaryColor: "#000",
-  secondaryColor: "#9747ff",
-  fadeColor: "#bcbcbc",
-  grayText: "#585858",
-};
