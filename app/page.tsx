cb894a480a47435a748a1abfca507794d268abfc
Dominik Stumpf <s.dominik.work@gmail.com>
2023-07-16 20:39:48 +0200

    separated global-styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 'use client';
 
+import { globalStyles } from './styles/global-styles';
+import { Global, css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 const Box = styled.div({
-  color: 'green',
+  color: 'paleturquoise',
   display: 'inline-block',
+  fontSize: 256,
 });
 
 export default function Home() {
   return (
     <main>
-      <Box>hello</Box>&nbsp;there
+      <Global styles={globalStyles} />
+      <Box>The quick brown fox jumps over the lazy dog</Box>
     </main>
   );
 }
---
+++ app/styles/global-styles.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const globalStyles = css({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+    textSizeAdjust: '100%',
+    textRendering: 'optimizeLegibility',
+    '-webkit-font-smoothing': 'antialiased',
+    fontFeatureSettings: '"rlig" 1,"calt" 0',
+  },
+  body: {
+    backgroundColor: 'black',
+    color: 'white',
+  },
+});
