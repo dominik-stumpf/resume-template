dafad67672add84a97a81bb2e749c1eb8d827c71
Dominik Stumpf <s.dominik.work@gmail.com>
2023-09-23 03:58:53 +0200

    nextjs config change

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    serverActions: true
-  }
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
