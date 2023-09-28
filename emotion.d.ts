a0d9a4790680f91432fa651e48183f887c3b31d3
Dominik Stumpf <s.dominik.work@gmail.com>
2023-09-28 02:32:47 +0200

    removed unused file

--- emotion.d.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeProps } from '@/src/styles/theme';
-import '@emotion/core';
-import '@emotion/react';
-import '@emotion/styled';
-
-declare module '@emotion/react' {
-  // rome-ignore lint/suspicious/noEmptyInterface: <explanation>
-  export interface Theme extends ThemeProps {}
-}
