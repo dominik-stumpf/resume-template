01cf58171b5bacc2f8542ebe663d37b1d140d78d
Dominik Stumpf <s.dominik.work@gmail.com>
2023-09-28 14:29:21 +0200

    print zoom out hack fix

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -17,15 +17,12 @@ const contacts: ContactProps[] = [
     icon: 'cib:github',
     href: 'github.com/dominik-stumpf',
   },
-  {
-    icon: 'cib:linkedin-in',
-    href: 'linkedin.com/in/dominik-stumpf',
-  },
+  { icon: 'cib:linkedin-in', href: 'linkedin.com/in/dominik-stumpf' },
 ];
 
 export function Contact() {
   return (
-    <address className='absolute flex gap-4 not-italic rotate-90 translate-x-1/2 -translate-y-1/2 right-3 top-1/2 whitespace-nowrap'>
+    <address className='fixed flex items-center justify-center gap-4 not-italic rotate-90 translate-x-1/2 -translate-y-1/2 right-3 top-1/2 w-max'>
       {contacts.map((contact) => (
         <div key={contact.href} className='flex items-center gap-2'>
           <Icon icon={contact.icon} className='w-3' />
